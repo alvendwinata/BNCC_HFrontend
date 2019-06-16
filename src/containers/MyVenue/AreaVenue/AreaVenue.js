9e6231043f7322806ae2de8eb7c2b1ec05b5a98c
Novi Steven <ns@Novis-MacBook-Pro.local>
2019-06-16 08:13:40 +0700

    area

--- src/containers/MyVenue/AreaVenue/AreaVenue.js
+++ src/containers/MyVenue/AreaVenue/AreaVenue.js
@@ -1,76 +1,123 @@
 import React, { useState } from "react";
 import { Upload, Button, Input, Icon, List } from "antd";
 import { connect } from "react-redux";
-import styles from './AreaVenue.module.css';
+import styles from "./AreaVenue.module.css";
 import * as actions from "../../../store/actions/index";
+
+import axios from "../../../axios";
 
 const { TextArea } = Input;
 
 const props = {
-    name: 'file',
-    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    name: "file",
+    action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
     headers: {
-      authorization: 'authorization-text',
-    },
-}
+        authorization: "authorization-text"
+    }
+};
 
-function AreaVenue({ user, onSubmit }) {
+function AreaVenue({ onSubmit, venueId }) {
     const [unitName, setUnitName] = useState("");
     const [unitPic, setUnitPic] = useState(null);
     const [unitData, setUnitData] = useState([]);
-    
-    const resetUnitForm = () => { 
-        setUnitName("")
-        setUnitPic(null)
-    }
+    const [areaName, setAreaName] = useState("");
+    const [areaDes, setAreaDes] = useState("");
+
+    const resetUnitForm = () => {
+        setUnitName("");
+        setUnitPic(null);
+    };
 
     const submitHandler = () => {
-        onSubmit();
-    }
+        const data = {
+            area: {
+                venueId: venueId,
+                name: areaName,
+                description: areaDes
+            }
+        };
 
-    const addUnitHandler = () => { 
-        const data = { 
-            unitName : unitName,
-            unitPic : unitPic
-        }
+        axios.post("/area/upsert", data).then(async res => {
+            if (res.data.success) {
+                let unit = {};
+                await unitData.forEach(async item => {
+                    unit = {
+                        unit: {
+                            areaId: res.data.area.id,
+                            name: item.unitName,
+                            pic: null
+                        }
+                    };
 
-        const restData = [...unitData]
-        restData.push(data)
-        setUnitData(restData)
-        resetUnitForm()
-    }
+                    await axios.post("/unit/upsert", unit);
+                });
 
-    const setUnitPicValue = value => { 
-        setUnitPic(value.file.name)
-    }
+                onSubmit();
+            }
+        });
+    };
 
-    const setUnitNameValue = value => { 
-        setUnitName(value.target.value)
-    }
+    const addUnitHandler = () => {
+        const data = {
+            unitName: unitName,
+            unitPic: unitPic
+        };
+
+        const restData = [...unitData];
+        restData.push(data);
+        setUnitData(restData);
+        resetUnitForm();
+    };
+
+    const setUnitPicValue = value => {
+        setUnitPic(value.file.name);
+    };
+
+    const setUnitNameValue = value => {
+        setUnitName(value.target.value);
+    };
 
     return (
-        <>            
+        <>
             <div className={styles.form_item}>
                 <div className={styles.form_label}>Area Name</div>
-                <Input className={styles.form_input} placeholder={"Ex: Football Court, Badminton Court, ..."} />
-            </div>            
+                <Input
+                    className={styles.form_input}
+                    placeholder={"Ex: Football Court, Badminton Court, ..."}
+                    value={areaName}
+                    onChange={e => setAreaName(e.target.value)}
+                />
+            </div>
             <div className={styles.form_item}>
                 <div className={styles.form_label}>Area Description</div>
-                <TextArea className={styles.form_input} />
+                <TextArea
+                    className={styles.form_input}
+                    value={areaDes}
+                    onChange={e => setAreaDes(e.target.value)}
+                />
             </div>
             <div className={styles.form_item}>
                 <div className={styles.box_form}>
                     <h3>Unit of Area</h3>
-                    <p>Unit of area describe your quantity of units in your area, which explains the definition of each of those unit.</p>
+                    <p>
+                        Unit of area describe your quantity of units in your
+                        area, which explains the definition of each of those
+                        unit.
+                    </p>
                     <div className={styles.box_form_item}>
                         <div className={styles.form_label}>Unit Name</div>
-                        <Input className={styles.form_input} placeholder={"Ex : Court C1, Court C2, ..."} value={unitName} onChange={setUnitNameValue} />
+                        <Input
+                            className={styles.form_input}
+                            placeholder={"Ex : Court C1, Court C2, ..."}
+                            value={unitName}
+                            onChange={setUnitNameValue}
+                        />
                     </div>
                     <div className={styles.box_form_item}>
                         <div className={styles.form_label}>Unit Picture</div>
                         <Upload {...props} onChange={setUnitPicValue}>
                             <Button>
-                            <Icon type="upload" /> Click to Upload
+                                <Icon type="upload" /> Click to Upload
                             </Button>
                         </Upload>
                     </div>
@@ -82,16 +129,16 @@ function AreaVenue({ user, onSubmit }) {
                         bordered
                         dataSource={unitData}
                         renderItem={item => (
-                            <List.Item>
-                                {item.unitName}
-                            </List.Item>
+                            <List.Item>{item.unitName}</List.Item>
                         )}
                     />
                 </div>
             </div>
 
             <div className={styles.form_item}>
-                <Button type="primary" onClick={submitHandler}>Add Area</Button>
+                <Button type="primary" onClick={submitHandler}>
+                    Add Area
+                </Button>
             </div>
         </>
     );
@@ -99,14 +146,17 @@ function AreaVenue({ user, onSubmit }) {
 
 const mapStateToProps = state => {
     return {
-        user: state.authReducer.user
-    }
-}
+        venueId: state.preferenceReducer.venueId
+    };
+};
 
 const mapDispatchToProps = dispatch => {
     return {
         onSubmit: () => dispatch(actions.updateCurrPref())
-    }
-}
+    };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(AreaVenue);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(AreaVenue);
