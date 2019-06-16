aaa4316d9a3b833c04bc0891fb326b08e9616f8e
Jacky <assangemaximus@gmail.com>
2019-06-16 07:24:33 +0700

    venue

--- src/containers/Venue/Venue.js
+++ src/containers/Venue/Venue.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { List, Collapse } from "antd";
 import styles from "./Venue.module.css";
 import LayoutPage from "../../hoc/LayoutPage/LayoutPage";
 
 const PAGE_NAME = "Venue";
+const Panel = Collapse.Panel;
 
 const venueConst = { 
     "name" : "PIK FIT",
@@ -21,10 +24,45 @@ const venueConst = {
             "sportsName" : "Volleyball",
         }
     ],
+    "facilities" : [{
+        "name" : "Kid Playground"
+    }, { 
+        "name" : "Shower"
+    }, {
+        "name" : "Wi-fi Access"
+    }],
+    "areas" : [
+        {
+            "areaID" : 1,
+            "areaName" : "Football Court",
+            "areaDesc" : "Court is usable for football sports, the court is made of rubber contain ...",
+            "units" : [
+                {
+                    "unitID" : 1,
+                    "unitName" : "Foot A Court",
+                },{
+                    "unitID" : 2,
+                    "unitName" : "Foot B Court"
+                }
+            ]
+        },{
+            "areaID" : 2,
+            "areaName" : "Tennis Court",
+            "areaDesc" : "Court is usable for Tennis sports, the court is made of rubber contain ...",
+            "units" : [
+                {
+                    "unitID" : 3,
+                    "unitName" : "Tennis A Court",
+                },{
+                    "unitID" : 4,
+                    "unitName" : "Tennis B Court"
+                }
+            ]
+        }
+    ],
     "address" : "Jalan nusa indah, jakarta utara",
     "city" : "Jakarta Utara",
     "desc" : "Tempat Berolahraga khusus untuk Gym Body Workout dengan fasilitas-fasilitas yang menjangkau ...",
-
 }
 
 function Venue({user}) { 
@@ -32,6 +70,60 @@ function Venue({user}) {
         <>
             <div className={styles.venue_pic}>
                 <img src={venueConst.pic} alt="venue-pic" />
+            </div>
+            <div className={styles.venue_title}>
+                {venueConst.name}
+            </div>
+            <div className={styles.venue_city}>
+                 {venueConst.city}
+            </div>
+            <div className={styles.venue_sports}>
+                {venueConst.sports.map(data => (
+                    <div className={styles.venue_sports_item}>{data.sportsName}</div>   
+                ))}
+            </div>
+            <div className={styles.box_container}>
+                 <div className={styles.box_title}>Overview</div>
+                 <div className={styles.box_item}>
+                    <div className={styles.box_item_label}>Description</div>
+                    <div className={styles.box_desc}>
+                        {venueConst.desc}
+                    </div>
+                 </div>
+                 <div className={styles.box_item}>
+                    <div className={styles.box_item_label}>Facilities</div>
+                    <div className={styles.box_desc}>
+                    <List
+                        dataSource={venueConst.facilities}
+                        renderItem={item => (
+                            <List.Item>
+                                {item.name}
+                            </List.Item>
+                        )}
+                    />
+                    </div>
+                 </div>
+            </div>
+            <div className={styles.box_container}>
+                 <div className={styles.box_title}>Area</div>
+                 <Collapse defaultActiveKey={keyJoin}>
+                    {
+                        areaData.map(data => (
+                            <Panel key={data.areaID} header={data.areaName}>
+                                <p>{data.areaDesc}</p>
+                                <List
+                                    bordered
+                                    dataSource={data.units}
+                                    renderItem={item => (
+                                        <List.Item>
+                                            {item.unitName}
+                                        </List.Item>
+                                    )}
+                                />
+                            </Panel>
+                        ))
+                    }
+                </Collapse>
             </div>
         </>
     );
